6cdbc91203e635e6c31e2ea1b375802b110ca81d
Kevin Palowa <kevinpalowa@gmail.com>
2023-05-18 20:34:36 +0700

    feat: add blog list

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,3 +1,28 @@
+import { Box, HStack, Heading, Link, Stack } from "@chakra-ui/react";
+import { useGetPosts } from "../hooks/post";
+import BlogListSkeleton from "../components/BlogListSkeleton";
+import { Link as ReactRouteLink } from "react-router-dom";
+import BlogList from "../components/BlogList";
+
 export default function Home() {
-  return <div>Home</div>;
+  const { data, isLoading } = useGetPosts();
+  return (
+    <Box maxW="600px" mx="auto" py={8} px={4}>
+      <Stack spacing={4}>
+        <HStack justifyContent="space-between">
+          <Heading as="h1" size="2xl">
+            My Blog
+          </Heading>
+          <Link as={ReactRouteLink} to="post/create">
+            + Add Post
+          </Link>
+        </HStack>
+        {isLoading && <BlogListSkeleton />}
+        {data &&
+          data.map((post) => (
+            <BlogList body={post.body} title={post.title} id={post.id} />
+          ))}
+      </Stack>
+    </Box>
+  );
 }
