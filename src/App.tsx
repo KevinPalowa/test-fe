32d192598583c0a02dccd44ab66bbaf0dccba2df
Kevin Palowa <kevinpalowa@gmail.com>
2023-05-18 20:34:26 +0700

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./page/Home";
+import Detail from "./page/post/Detail";
+import Create from "./page/post/Create";
+import Edit from "./page/post/Edit";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Home />} index />
+      <Route path="post">
+        <Route path=":id">
+          <Route index element={<Detail />} />
+          <Route path="edit" element={<Edit />} />
+        </Route>
+        <Route path="create" element={<Create />} />
+      </Route>
     </Routes>
   );
 }
