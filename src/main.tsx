32dd3597f4829b4e704e3a88e2a2c6d928e91067
Kevin Palowa <kevinpalowa@gmail.com>
2023-05-18 16:45:13 +0700

    chore: initial setup

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./lib/reactQueryClient.ts";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ChakraProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
