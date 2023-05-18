0bb90cc1dca3bc1bf3da74b0c145df6f8cd8c88b
Kevin Palowa <kevinpalowa@gmail.com>
2023-05-18 16:51:17 +0700

    chore: install react query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./lib/reactQueryClient.ts";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <ChakraProvider>
         <BrowserRouter>
           <App />
